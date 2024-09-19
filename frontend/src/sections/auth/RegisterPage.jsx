d70fc8d027efece5edf2beb5a4eb05667da70fde
Paulajungaker <paula010514@hotmail.com>
2024-09-19 18:25:03 +0200

    Fixing spelling issue and trigger new deploy

--- frontend/src/sections/auth/RegisterPage.jsx
+++ frontend/src/sections/auth/RegisterPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useAuth } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
-import "./styling/authPages.css";
+import "./styling/AuthPages.css";
 
 const RegisterPage = () => {
   const [username, setUsername] = useState("");
